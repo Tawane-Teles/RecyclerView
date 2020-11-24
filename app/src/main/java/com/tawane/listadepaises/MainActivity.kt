62c4d8145fb22f1c49d6b2344315810bd0259245
Tawane <tawanesilva.96@hotmail.com>
2020-11-24 17:50:39 -0300

    adicionando imagens e identificando com os nomes dos paises

--- app/src/main/java/com/tawane/listadepaises/MainActivity.kt
+++ app/src/main/java/com/tawane/listadepaises/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.tawane.listadepaises
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
+
+    var nameList = ArrayList<String>()
+    var listaAdapter: MainActivity? = null
+    var linearLayoutManager: LinearLayoutManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+        getCats()
+
+        btnInsert.setOnClickListener {
+            val intent = Intent(this, NameActivity::class.java)
+            startActivityForResult(intent, 1)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initView()
+    }
+
+    private fun initView() {
+        listaAdapter = ListaAdapter(nameList, this)
+        linearLayoutManager = LinearLayoutManager(this)
+        recyclerview.layoutManager = linearLayoutManager
+        recyclerview.adapter = listaAdapter
+    }
+
+    // setando no adapter os gatos
+    private fun getCats() {
+        nameList.add(getString(R.string.cat1))
+        nameList.add(getString(R.string.cat2))
+    }
+
+    //Buscando os gastos que inseriou na tela Name
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            val nome: String = data!!.getStringExtra("nome").toString()
+            nameList.add(nome)
+        }
     }
 }
---
+++ app/src/main/java/com/tawane/listadepaises/adapter/ListaAdapter.kt
@@ -0,0 +1,65 @@
+package com.tawane.listadepaises.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.tawane.listadepaises.R
+import com.tihasg.mycatslist.R
+import kotlinx.android.synthetic.main.content_lista.view.*
+
+class ListaAdapter(nameList: List<String>, private var ctx: Context) :
+    RecyclerView.Adapter<ListaAdapter.ViewHolder>() {
+
+    private var nameList: List<String> = ArrayList<String>()
+
+    init {
+        this.nameList = nameList
+    }
+
+    // Aqui é onde o viewholder é criado a partir do layout
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(ctx).inflate(R.layout.content_lista, parent, false)
+        return RecyclerView.ViewHolder(view)
+    }
+
+    // Nessa parte é onde se modifica o item do viewholder
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val name = nameList[position]
+
+        holder.name.text = name
+
+        // Verifica se o nome for igual a Emy seta a cor e a imagem em si
+        if (name == ctx.resources.getString(R.string.pais_um)) {
+            holder.name.setTextColor(ContextCompat.getColor(ctx, R.color.green))
+            holder.imagem.setBackgroundResource(R.drawable.img_brazil)
+        }
+
+        if (name == ctx.resources.getString(R.string.pais_dois)) {
+            holder.name.setTextColor(ContextCompat.getColor(ctx, R.color.red))
+            holder.imagem.setBackgroundResource(R.drawable.img_estadosunidos)
+        }
+        if (name == ctx.resources.getString(R.string.pais_tres)) {
+            holder.name.setTextColor(ContextCompat.getColor(ctx, R.color.dark_red))
+            holder.imagem.setBackgroundResource(R.drawable.img_canada)
+        }
+        if (name == ctx.resources.getString(R.string.pais_quatro)) {
+            holder.name.setTextColor(ContextCompat.getColor(ctx, R.color.blue))
+            holder.imagem.setBackgroundResource(R.drawable.img_franca)
+    }
+
+    // Devolve quantidade de itens do nameList
+    override fun getItemCount(): Int {
+        return nameList.size
+    }
+
+    // Aqui é a criação dos itens do viewholder
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var name = view.findViewById<TextView>(R.id.img_primeirafrase)!!
+        var imagem = view.findViewById<ImageView>()!!
+    }
+}
